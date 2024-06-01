a8e7b11041b36543c475491c2c0a9dbddcceae30
Даниил <danilazakharov2003@gmail.com>
2024-06-01 13:37:57 +0300

    initial commit

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,72 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    //@Test
+    //public void shouldThrowAnExceptionIfNegativeRate() {
+
+    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+    //        CreditAccount account = new CreditAccount(
+    //                0,
+    //                5_000,
+    //                -15
+    //        );
+    //    } );
+   // }
+
+    //@Test
+    //public void shouldThrowAnExceptionIfNegativeInitialBalance() {
+
+    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+    //        CreditAccount account = new CreditAccount(
+    //                -1_000,
+    //                5_000,
+    //                15
+    //        );
+    //    } );
+    //}
+
+    //@Test
+    //public void shouldThrowAnExceptionIfNegativeCreditLimit() {
+
+    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
+    //        CreditAccount account = new CreditAccount(
+    //                0,
+    //                -5_000,
+    //                15
+    //        );
+    //    } );
+   // }
+
+
+
+    //@Test
+    //public void showBalanceAfterPurchase(){
+    //    CreditAccount account = new CreditAccount(
+    //            0,
+    //            5_000,
+    //            15
+    //    );
+
+    //    account.add(3_000);
+    //    account.pay(1_000);
+
+    //    Assertions.assertEquals(2_000, account.getBalance());
+    //}
+
+    //@Test
+    //public void showBalanceAfterPurchaseExceedingBalance(){
+    //    CreditAccount account = new CreditAccount(
+    //            0,
+    //            5_000,
+    //            15
+    //    );
+
+    //    account.add(3_000);
+    //    account.pay(4_000);
+
+    //    Assertions.assertEquals(3_000, account.getBalance());
+    //}
+    //.g
+    // 31232131321313
 }

fb41e3fe97ad7128c78f5c12ec273514eafd3608
Даниил <danilazakharov2003@gmail.com>
2024-06-06 21:10:28 +0300

    bug fixes

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -18,71 +18,136 @@ public class CreditAccountTest {
         Assertions.assertEquals(3_000, account.getBalance());
     }
 
-    //@Test
-    //public void shouldThrowAnExceptionIfNegativeRate() {
+    @Test
+    public void shouldNotChangeTheBalanceIfTheValueIsIncorrect() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
 
-    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
-    //        CreditAccount account = new CreditAccount(
-    //                0,
-    //                5_000,
-    //                -15
-    //        );
-    //    } );
-   // }
+        account.add(-3_000);
 
-    //@Test
-    //public void shouldThrowAnExceptionIfNegativeInitialBalance() {
+        Assertions.assertEquals(0, account.getBalance());
+    }
 
-    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
-    //        CreditAccount account = new CreditAccount(
-    //                -1_000,
-    //                5_000,
-    //                15
-    //        );
-    //    } );
-    //}
+    @Test
+    public void shouldThrowAnExceptionIfNegativeRate() {
 
-    //@Test
-    //public void shouldThrowAnExceptionIfNegativeCreditLimit() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    0,
+                    5_000,
+                    -15
+            );
+        });
+    }
 
-    //    Assertions.assertThrows(IllegalArgumentException.class, () -> {
-    //        CreditAccount account = new CreditAccount(
-    //                0,
-    //                -5_000,
-    //                15
-    //        );
-    //    } );
-   // }
+    @Test
+    public void shouldThrowAnExceptionIfNegativeInitialBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    -1_000,
+                    5_000,
+                    15
+            );
+        });
+    }
+
+    @Test
+    public void shouldThrowAnExceptionIfNegativeCreditLimit() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            CreditAccount account = new CreditAccount(
+                    0,
+                    -5_000,
+                    15
+            );
+        });
+    }
+
+    @Test
+    public void mustCreateCreditAccountSpecifiedParameters() {
+        CreditAccount account = new CreditAccount(
+                2_000,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(2_000, account.getBalance());
+        Assertions.assertEquals(5_000, account.getCreditLimit());
+        Assertions.assertEquals(15, account.getRate());
 
 
+    }
 
-    //@Test
-    //public void showBalanceAfterPurchase(){
-    //    CreditAccount account = new CreditAccount(
-    //            0,
-    //            5_000,
-    //            15
-    //    );
 
-    //    account.add(3_000);
-    //    account.pay(1_000);
+    @Test
+    public void showBalanceAfterPurchase() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
 
-    //    Assertions.assertEquals(2_000, account.getBalance());
-    //}
+        account.add(3_000);
+        account.pay(1_000);
 
-    //@Test
-    //public void showBalanceAfterPurchaseExceedingBalance(){
-    //    CreditAccount account = new CreditAccount(
-    //            0,
-    //            5_000,
-    //            15
-    //    );
+        Assertions.assertEquals(2_000, account.getBalance());
+     }
 
-    //    account.add(3_000);
-    //    account.pay(4_000);
+    @Test
+    public void showBalanceAfterPurchaseExceedingBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                100,
+                15
+        );
 
-    //    Assertions.assertEquals(3_000, account.getBalance());
-    //}
-    //.g
-    // 31232131321313
+        account.add(3_000);
+        account.pay(4_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+     }
+
+    @Test
+    public void shouldNotChangeTheBalanceIfThePurchaseValueIsIncorrect() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.pay(-100);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void mustCalculateInterestOnNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                -100,
+                0,
+                15
+        );
+
+        int expected = -15;
+        Assertions.assertEquals(-15, account.yearChange());
+
+    }
+
+    @Test
+    public void shouldNotCountInterestIfTheBalanceIsNonNegative() {
+        CreditAccount account = new CreditAccount(
+                100,
+                0,
+                15
+        );
+
+        int expected = -15;
+        Assertions.assertEquals(0, account.yearChange());
+
+
+    }
 }
--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -3,19 +3,22 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//public class SavingAccountTest {
+public class SavingAccountTest {
 
-    // @Test
-    //public void shouldAddLessThanMaxBalance() {
-    //    SavingAccount account = new SavingAccount(
-    //            2_000,
-    //            1_000,
-    //            10_000,
-    //            5
-    //    );
+ @Test
+public void shouldAddLessThanMaxBalance() {
+    SavingAccount account = new SavingAccount(
+            2_000,
+            1_000,
+            10_000,
+            5
+    );
 
-    //    account.add(3_000);
+    account.add(3_000);
 
-    //    Assertions.assertEquals(2_000 + 3_000, account.getBalance());
-    //}
-//}
+    Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+}
+
+
+}
+

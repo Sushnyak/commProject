4a2e8248f28f0d946114b523ed754c64b62ea561
Maria Gamzina <gamzinamaria@gmail.com>
2024-06-07 15:39:08 +0300

    change code3

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -18,17 +18,5 @@ public class CreditAccountTest {
         Assertions.assertEquals(3_000, account.getBalance());
     }
 
-    @Test
-    public void showBalanceAfterPurchaseExceedingBalance() {
-        CreditAccount account = new CreditAccount(
-                0,
-                100,
-                15
-        );
 
-        account.add(3_000);
-        account.pay(4_000);
-
-        Assertions.assertEquals(0, account.getBalance());
-    }
 }
